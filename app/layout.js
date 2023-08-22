c6376ba40543c8003e2b0a3ee7d0a3018efccb82
KobyS82 <116032130+KobyS82@users.noreply.github.com>
2023-08-21 19:54:38 -0500

    relearning flexbox

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- app/page.js
+++ app/page.js
@@ -1,9 +1,41 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="bg-slate-400 text-center">
+      <header>
+        <h1 className="p-4">Chore Tracker!</h1>
+      </header>
+      <div className="flex flex-col ">
+        <ul className="flex bg-slate-900 w-screen flex-col content-center text-center">
+          <li>Wash Dishes</li>
+          <li>Take out Trash</li>
+          <li>Feed the Dog</li>
+        </ul>
+        <ul className="flex bg-slate-800 w-screen flex-col text-center">
+          <li>Test Wash Dishes</li>
+          <li>Test Take out Trash</li>
+          <li>Test Feed the Dog</li>
+        </ul>
+        
+      </div>
     </main>
-  )
+    
+  );
 }
+
+
+// <main className="flex m-1 flex-col">
+//       <div className="flex justify-center bg-slate-800">
+//         <h1>Chore Tracker!</h1>
+//       </div>
+//       <div className="self-center">
+//         <h2>Chores:</h2>
+//         <ul className="content-center">
+//           <li>Wash Dishes</li>
+//           <li>Take out Trash</li>
+//           <li>Feed the Dog</li>
+//         </ul>
+//       </div>
+//         <div className="flex justify-center">
+//           <h2>Chore List</h2>
+//         </div>
+//     </main>
